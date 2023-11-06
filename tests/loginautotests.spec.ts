6d6eb2f756cce3daae733a9b7c689b7e7c1c6fb9
Martín Gil Navarro <70269740+martingilnavarro@users.noreply.github.com>
2023-11-06 08:57:52 -0300

    login negative tests added

--- tests/loginautotests.spec.ts
+++ tests/loginautotests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('log in', () => {
+test.describe('log in should be allowed', () => {
   
   test.beforeEach(async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
@@ -30,5 +30,46 @@ test.describe('log in', () => {
     // Input username.
     await page.getByPlaceholder('Username').fill('locked_out_user');
   });
+
+});
+
+test.describe('log in should not be allowed', () => {
+  
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+  });
+  test.afterEach(async ({ page }) => {    
+    // Click login.
+    await page.getByText('Login').click();
+      
+    // Same page - does not go to dashboard.
+    await expect(page).toHaveURL('https://www.saucedemo.com/')
+  });
+
+  test('log in - user name incorrect', async ({ page }) => {
+    // Input no existing username.
+    await page.getByPlaceholder('Username').fill('no_user');
+    // Input password.
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+  });
+
+  test('log in - password incorrect', async ({ page }) => {
+    // Input username.
+    await page.getByPlaceholder('Username').fill('standard_user');
+    // Input incorrect password.
+    await page.getByPlaceholder('Password').fill('incorrect_password');
+  });
+
+  test('log in - no username', async ({ page }) => {
+
+    // Input only password.
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+  });
+
+  test('log in - no password', async ({ page }) => {
+    // Input only username.
+    await page.getByPlaceholder('Username').fill('standard_user');
+  });
+
 
 });
