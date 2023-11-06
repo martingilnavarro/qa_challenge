6510d713f9291e3dea919a04e6d3c8d7cb237c06
Martín Gil Navarro <70269740+martingilnavarro@users.noreply.github.com>
2023-11-06 15:47:41 -0300

    locators sintaxis changed and github actions added

--- tests/loginautotests.spec.ts
+++ tests/loginautotests.spec.ts
@@ -7,10 +7,10 @@ test.describe('log in should be allowed', () => {
   });
   test.afterEach(async ({ page }) => {
     // Input password.
-    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
     
     // Click login.
-    await page.getByText('Login').click();
+    await page.locator('[data-test="login-button"]').click();
       
     // Dashboard page.
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
@@ -18,17 +18,17 @@ test.describe('log in should be allowed', () => {
 
   test('log in - pre prod', async ({ page }) => {
     // Input username.
-    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.locator('[data-test="username"]').fill('standard_user');
   });
 
   test('log in - QA', async ({ page }) => {
     // Input username.
-    await page.getByPlaceholder('Username').fill('problem_user');
+    await page.locator('[data-test="username"]').fill('problem_user');
   });
 
   test('log in - dev', async ({ page }) => {
     // Input username.
-    await page.getByPlaceholder('Username').fill('locked_out_user');
+    await page.locator('[data-test="username"]').fill('locked_out_user');
   });
 
 });
@@ -40,7 +40,7 @@ test.describe('log in should not be allowed', () => {
   });
   test.afterEach(async ({ page }) => {    
     // Click login.
-    await page.getByText('Login').click();
+    await page.locator('[data-test="login-button"]').click();
       
     // Same page - does not go to dashboard.
     await expect(page).toHaveURL('https://www.saucedemo.com/')
@@ -48,27 +48,27 @@ test.describe('log in should not be allowed', () => {
 
   test('log in - user name incorrect', async ({ page }) => {
     // Input no existing username.
-    await page.getByPlaceholder('Username').fill('no_user');
+    await page.locator('[data-test="username"]').fill('no_user');
     // Input password.
-    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
   });
 
   test('log in - password incorrect', async ({ page }) => {
     // Input username.
-    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.locator('[data-test="username"]').fill('standard_user');
     // Input incorrect password.
-    await page.getByPlaceholder('Password').fill('incorrect_password');
+    await page.locator('[data-test="password"]').fill('incorrect_password');
   });
 
   test('log in - no username', async ({ page }) => {
 
     // Input only password.
-    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
   });
 
   test('log in - no password', async ({ page }) => {
     // Input only username.
-    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.locator('[data-test="username"]').fill('standard_user');
   });
 
 
